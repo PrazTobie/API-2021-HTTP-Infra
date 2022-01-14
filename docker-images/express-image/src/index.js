9356dda587a81d2d7afaedb1f2e45b80b59a7090
PrazTobie <tobie.praz@heig-vd.ch>
2022-01-14 10:11:40 +0100

    Traefik + Portainer

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}]`, req.path);
+    next();
+});
+
 app.get("/", (req, res) => {
     res.json({ random: Math.random() });
 });
 
-app.listen(3000);
+
+app.listen(3000, () => {
+    console.log("API Ready");
+});
